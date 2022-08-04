fa37e0bfbc79f2d82ba503d3fe8085d75197b647
Anna-Anka <anjbekrenewa@gmail.com>
2022-08-04 15:53:41 +0300

    added studio

--- app/js/main.js
+++ app/js/main.js
@@ -2,4 +2,27 @@ document.addEventListener('DOMContentLoaded', () => {
     if (document.querySelector('.form__field--phone')) {
          $('.form__field--phone').inputmask();
     }
+
+    if (document.querySelector('.gallery')) {
+        const mediaQuery = window.matchMedia('(max-width: 1000px)')
+
+        function handleTabletChange(e) {
+            if (e.matches) {
+                console.log('dsada')
+                const sliderStudio = document.querySelector('.studio__swiper');
+                const swiperStudio = new Swiper(sliderStudio, {
+                    slidesPerView: 1,
+                    slidesPerGroup: 1,
+                    spaceBetween: 0,
+
+                    navigation: {
+                        nextEl: '.gallery__button--next',
+                        prevEl: '.gallery__button--prev',
+                    },
+                });
+            }
+        }
+        mediaQuery.addListener(handleTabletChange)
+        handleTabletChange(mediaQuery)
+    }
 });
--- gulpfile.js
+++ gulpfile.js
@@ -97,6 +97,7 @@ function scripts() {
     return src([
             'node_modules/jquery/dist/jquery.js',
             'node_modules/maskedinput/dist/min/jquery.inputmask.bundle.min.js',
+            'node_modules/swiper/swiper-bundle.min.js',
             /*'node_modules/slick-carousel/slick/slick.js',
             'node_modules/mixitup/dist/mixitup.min.js',
             'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
