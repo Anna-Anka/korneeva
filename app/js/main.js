3144ff6fad504ad2965779698f54d455e9840d41
Anna-Anka <anjbekrenewa@gmail.com>
2022-08-04 18:04:38 +0300

    addeв reviews

--- app/js/main.js
+++ app/js/main.js
@@ -3,12 +3,25 @@ document.addEventListener('DOMContentLoaded', () => {
          $('.form__field--phone').inputmask();
     }
 
+    if (document.querySelector('.reviews')) {
+        const sliderReviews = document.querySelector('.reviews__swiper');
+        const swiperReviews = new Swiper(sliderReviews, {
+            slidesPerView: 1,
+            slidesPerGroup: 1,
+            spaceBetween: 0,
+
+            navigation: {
+                nextEl: '.reviews__button--next',
+                prevEl: '.reviews__button--prev',
+            },
+        });
+    }
+
     if (document.querySelector('.gallery')) {
         const mediaQuery = window.matchMedia('(max-width: 1000px)')
 
         function handleTabletChange(e) {
             if (e.matches) {
-                console.log('dsada')
                 const sliderStudio = document.querySelector('.studio__swiper');
                 const swiperStudio = new Swiper(sliderStudio, {
                     slidesPerView: 1,
