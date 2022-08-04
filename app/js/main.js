e73d1b81767a9a438cea36c499bbbf8b0d1914b4
Anna-Anka <anjbekrenewa@gmail.com>
2022-08-04 10:40:20 +0300

    added enroll

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,5 @@
-$(function(){
-
+document.addEventListener('DOMContentLoaded', () => {
+    if (document.querySelector('.form__field--phone')) {
+         $('.form__field--phone').inputmask();
+    }
 });
