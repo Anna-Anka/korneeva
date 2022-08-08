97e697a41d1b9d78b693902960dc9a729946033b
Anna-Bekreneva <anjabekreneva@gmail.com>
2022-08-08 11:41:25 +0300

    added correct

--- app/js/main.js
+++ app/js/main.js
@@ -3,6 +3,16 @@ document.addEventListener('DOMContentLoaded', () => {
         $('.form__field--phone').inputmask();
     }
 
+    if (document.querySelector('.reviews')) {
+        $(".reviews__stars").rateYo({
+            starWidth: "28px",
+            ratedFill: "#FECC02",
+            readOnly: true,
+            starSvg: '<svg width="28" height="26" viewBox="0 0 28 26" fill="none" xmlns="http://www.w3.org/2000/svg"><path d = "M14 0L17.1432 9.67376H27.3148L19.0858 15.6525L22.229 25.3262L14 19.3475L5.77101 25.3262L8.9142 15.6525L0.685208 9.67376H10.8568L14 0Z"/></svg>',
+            spacing: "3px"
+        });
+    }
+
     if (document.querySelector('.modal')) {
         const modalOpen = document.querySelectorAll('.modal-open');
         modalOpen.forEach(item => {
@@ -29,16 +39,6 @@ document.addEventListener('DOMContentLoaded', () => {
             slidesPerView: 1,
             slidesPerGroup: 1,
             spaceBetween: 0,
-
-            breakpoints: {
-                1000: {
-                    allowTouchMove: true,
-                },
-
-                360: {
-                    allowTouchMove: false,
-                },
-            },
 
             navigation: {
                 nextEl: '.reviews__button--next',
--- gulpfile.js
+++ gulpfile.js
@@ -98,12 +98,7 @@ function scripts() {
             'node_modules/jquery/dist/jquery.js',
             'node_modules/maskedinput/dist/min/jquery.inputmask.bundle.min.js',
             'node_modules/swiper/swiper-bundle.min.js',
-            /*'node_modules/slick-carousel/slick/slick.js',
-            'node_modules/mixitup/dist/mixitup.min.js',
-            'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
             'node_modules/rateyo/src/jquery.rateyo.js',
-            'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
-            'node_modules/jquery-form-styler/dist/jquery.formstyler.js',*/
             'app/js/main.js'
         ])
         .pipe(concat('main.min.js'))
